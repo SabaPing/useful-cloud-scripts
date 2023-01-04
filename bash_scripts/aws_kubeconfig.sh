4a69461e645c8e35a7630c1895de4d2d5a36060c
Yifan Xu <xuyifan02@pingcap.com>
2023-01-04 12:05:00 +0800

    aws kubeconfig.sh

--- bash_scripts/aws_kubeconfig.sh
+++ bash_scripts/aws_kubeconfig.sh
@@ -3,13 +3,14 @@ set -euo pipefail
 
 unset KUBECONFIG
 
-# Make sure to config SSO and profile in ~/.aws/config
-awsProfile=( auto-dev-rw )
+# Make sure to config SSO and profile in ~/.aws/config.
+# Make sure to login with aws sso login.
+awsProfile=( auto-dev-rw auto-prod-rw)
 
 awsRegions=( ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-3 eu-central-1 us-east-1 us-west-1 us-west-2 )
 
 function awsKubeConfig(){
-    aws eks --profile $1 --region $2 --output text list-clusters | awk '{print $2}' | xargs -n 1 -I {} aws eks --profile $1 --region $2 update-kubeconfig --kubeconfig aws.kubeconfig.yaml --name {}
+    aws eks --profile $1 --region $2 --output text list-clusters | awk '{print $2}' | xargs -n 1 -I {} aws eks --profile $1 --region $2 update-kubeconfig --kubeconfig output/aws.kubeconfig.yaml --name {} || true
 }
 
 # update kubeconfig for AWS EKS cluster
