047f02d97a98ae52c0a8f01bd0dc1e3c4dad0f55
Yifan Xu <xuyifan02@pingcap.com>
2023-01-04 13:11:28 +0800

    fix gcloud env

--- bash_scripts/gcp_kubeconfig.sh
+++ bash_scripts/gcp_kubeconfig.sh
@@ -9,13 +9,16 @@ projects=( gcp-autonomous-dev gcp-autonomous-prod)
 gcpRegions=( asia-east1 asia-northeast1 asia-northeast2 asia-southeast1 us-central1 us-east4 us-west1 )
 
 function gcpKubeConfig(){
-    KUBECONFIG=output/gcp.kubeconfig.yaml gcloud container clusters list --project $1 --region $2 | awk 'NR == 1 {next} {print $1}' | xargs -n 1 -I {} gcloud container clusters get-credentials {} --project $1 --region $2 || true
+    gcloud container clusters list --project $1 --region $2 | awk 'NR == 1 {next} {print $1}' | xargs -n 1 -I {} gcloud container clusters get-credentials {} --project $1 --region $2 || true
 }
 
 touch output/gcp.kubeconfig.yaml
+export KUBECONFIG=output/gcp.kubeconfig.yaml
 
 for project in "${projects[@]}"; do
     for region in "${gcpRegions[@]}"; do
         gcpKubeConfig $project $region
     done
 done
+
+unset KUBECONFIG
