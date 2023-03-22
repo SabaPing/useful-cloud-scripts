29e93b87fd2d9f03a89388103dba899809e7d134
Yifan Xu <xuyifan02@pingcap.com>
2023-03-22 10:40:32 +0800

    make scripts more robust

--- bash_scripts/aws_kubeconfig.sh
+++ bash_scripts/aws_kubeconfig.sh
@@ -6,11 +6,12 @@ unset KUBECONFIG
 # Make sure to config SSO and profile in ~/.aws/config.
 # Make sure to login with aws sso login (`aws sso login --profile xxxx`).
 awsProfile=( auto-dev-rw auto-prod-rw)
-
 awsRegions=( ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-3 eu-central-1 us-east-1 us-west-1 us-west-2 )
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 function awsKubeConfig(){
-    aws eks --profile $1 --region $2 --output text list-clusters | awk '{print $2}' | xargs -n 1 -I {} aws eks --profile $1 --region $2 update-kubeconfig --kubeconfig output/aws.kubeconfig.yaml --name {} || true
+    aws eks --profile $1 --region $2 --output text list-clusters | awk '{print $2}' | xargs -n 1 -I {} aws eks --profile $1 --region $2 update-kubeconfig --kubeconfig $SCRIPT_DIR/output/aws.kubeconfig.yaml --name {} || true
 }
 
 # update kubeconfig for AWS EKS cluster
--- bash_scripts/gcp_kubeconfig.sh
+++ bash_scripts/gcp_kubeconfig.sh
@@ -5,15 +5,17 @@ unset KUBECONFIG
 
 # Make sure to login to gcloud first (`gcloud auth application-default login`).
 projects=( gcp-autonomous-dev gcp-autonomous-prod)
-
 gcpRegions=( asia-east1 asia-northeast1 asia-northeast2 asia-southeast1 us-central1 us-east4 us-west1 )
 
 function gcpKubeConfig(){
     gcloud container clusters list --project $1 --region $2 | awk 'NR == 1 {next} {print $1}' | xargs -n 1 -I {} gcloud container clusters get-credentials {} --project $1 --region $2 || true
 }
 
-touch output/gcp.kubeconfig.yaml
-export KUBECONFIG=output/gcp.kubeconfig.yaml
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+OUTPUT_DIR=$SCRIPT_DIR/output
+mkdir -p $OUTPUT_DIR
+touch $OUTPUT_DIR/gcp.kubeconfig.yaml
+export KUBECONFIG=$OUTPUT_DIR/gcp.kubeconfig.yaml
 
 for project in "${projects[@]}"; do
     for region in "${gcpRegions[@]}"; do
